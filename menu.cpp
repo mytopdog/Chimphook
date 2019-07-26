356f9e3dac91e760fc90e35ee64d2429b4a425c3
Elliott Eastham <s17083@crgs.org.uk>
2019-07-26 23:11:28 +0100

    kill yourself

--- menu.cpp
+++ menu.cpp
@@ -1495,7 +1495,7 @@ std::vector<ImTextureID> Menu::Render(IDirect3DDevice9* pDevice)
 					Settings::System::Unload = true;
 
 				break;
-			// your mom are gey
+			// your mom are gey ;(
 			}
 		}
 		ImGui::EndGroupBox();
